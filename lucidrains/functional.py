fc9008ebe4af5e5a32540f7aea3ac5127dc045a4
David Marx <david.marx84@gmail.com>
2022-10-10 15:00:38 -0700

    moving stuff

--- lucidrains/functional.py
+++ lucidrains/functional.py
@@ -5,40 +5,7 @@ from imagen_pytorch import (
     maybe,
     once,
     default,
-    pad_tuple_to_length
-    Always
+    pad_tuple_to_length,
+    Always,
 )
 
-# misc
-from imagen_pytorch import (
-    print_once,
-  resize_image_to,
-  normalize_neg_one_to_one,
-  unnormalize_zero_to_one,
-)
-
-# typing stuff
-from imagen_pytorch import (
-
-    cast_tuple,
-    is_float_dtype,
-    cast_uint8_images_to_float,
-  
-)
-
-#nn
-from imagen_pytorch import (
-  module_device,
-  zero_init_,
-  eval_decorator,
-  Identity,
-  log,
-  l2norm,
-  right_pad_dims_to,
-  masked_mean,
-  prob_mask_like,
-  LayerNorm,
-  ChanLayerNorm,
-  Residual,
-  Parallel,
-)
